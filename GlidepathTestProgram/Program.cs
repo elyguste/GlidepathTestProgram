fa748f31fa18e182c817f84fdd5ca7bfb4b7d608
elyguste <guste.ely21@gmail.com>
2023-02-03 14:27:15 +1300

    read/write on a file code

--- GlidepathTestProgram/Program.cs
+++ GlidepathTestProgram/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace GlidepathTestProgram
+{
+    class Program
+    {
+        // Main entry
+        
+        static void Main(string[] args)
+        {
+            ReadWriteFunctions rwFunction = new ReadWriteFunctions();
+            int inputNumber = UserInput();
+
+            ReadWriteFunctions.Wrtie(inputNumber);
+            string last = ReadWriteFunctions.Read();
+        }
+
+        public static int UserInput()
+        {
+            Console.Write("Please enter number: ");
+
+            int number = Convert.ToInt32(userInput());
+
+            return number;
+        }
+
+        public static string userInput()
+        {
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input) || !int.TryParse(input, out int value))
+            {
+                Console.WriteLine("Please enter numeric value only");
+                return input = Console.ReadLine();
+            }
+            else
+                return input;
+        }
+    }
+}
---
+++ GlidepathTestProgram/ReadWriteFunctions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace GlidepathTestProgram
+{
+    public class ReadWriteFunctions
+    {  
+        public static void Wrtie(int number)
+        {
+            using (StreamWriter writer = new StreamWriter("Wfile.txt", true))
+            {
+                writer.WriteLine(number);
+            }
+        }
+
+        public static string Read()
+        {
+            String lastRow = File.ReadLines("Wfile.txt").Last();
+            return lastRow;
+
+        }
+    }
+}
